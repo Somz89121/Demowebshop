6813789901377f5eaf1ba7ac49ff212de557c6ec
Somz89121 <135639555+Somz89121@users.noreply.github.com>
2025-04-14 11:32:31 +0530

    Module chanfe

--- pageobject/test_computers.py
+++ pageobject/test_computers.py
@@ -1,7 +1,7 @@
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-from helpers.test_commonmethods import Helpers
+from TDD.helpers.test_commonmethods import Helpers
 
 helper = Helpers()
 
